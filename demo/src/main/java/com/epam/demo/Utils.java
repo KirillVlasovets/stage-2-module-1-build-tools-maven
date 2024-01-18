06da0e3306f1d49a09bcb00f30c780e790b47162
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-01-18 20:44:29 +0400

    solution added

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        for (String str : args) {
+            double number;
+            if (str.contains("0")) {
+                return false;
+            }
+            try {
+                number = Double.parseDouble(str);
+            } catch (NumberFormatException | NullPointerException exception) {
+                return false;
+            }
+            if (number <= 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        double number;
+        try {
+            number = Double.parseDouble(str);
+            if (str.startsWith("0")) {
+                return false;
+            }
+        } catch (NumberFormatException | NullPointerException exception) {
+            return false;
+        }
+        return number > 0;
     }
 }
